3c76972a347544661ecff3b76eb7e4ffe46d4c1f
Benny Daon <benny@tuzig.com>
2020-04-17 10:16:58 +0300

    Fixing the test

--- echo_test.go
+++ echo_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/pion/webrtc/v2"
 	"testing"
+	"time"
 )
 
 // expectedLabel represents the label of the data channel we are trying to test.
@@ -11,8 +12,62 @@ import (
 // bindings this is a requirement).
 const expectedLabel = "data"
 
-func TestSimpleEcho(t *testing.T) {
+func signalPair(pcOffer *webrtc.PeerConnection, pcAnswer *webrtc.PeerConnection) error {
+	iceGatheringState := pcOffer.ICEGatheringState()
+	offerChan := make(chan webrtc.SessionDescription, 1)
 
+	if iceGatheringState != webrtc.ICEGatheringStateComplete {
+		pcOffer.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+			if candidate == nil {
+				offerChan <- *pcOffer.PendingLocalDescription()
+			}
+		})
+	}
+	// Note(albrow): We need to create a data channel in order to trigger ICE
+	// candidate gathering in the background for the JavaScript/Wasm bindings. If
+	// we don't do this, the complete offer including ICE candidates will never be
+	// generated.
+	if _, err := pcOffer.CreateDataChannel("webexec", nil); err != nil {
+		return err
+	}
+
+	offer, err := pcOffer.CreateOffer(nil)
+	if err != nil {
+		return err
+	}
+	if err := pcOffer.SetLocalDescription(offer); err != nil {
+		return err
+	}
+
+	if iceGatheringState == webrtc.ICEGatheringStateComplete {
+		offerChan <- offer
+	}
+	select {
+	case <-time.After(3 * time.Second):
+		return fmt.Errorf("timed out waiting to receive offer")
+	case offer := <-offerChan:
+		if err := pcAnswer.SetRemoteDescription(offer); err != nil {
+			return err
+		}
+
+		answer, err := pcAnswer.CreateAnswer(nil)
+		if err != nil {
+			return err
+		}
+
+		if err = pcAnswer.SetLocalDescription(answer); err != nil {
+			return err
+		}
+
+		err = pcOffer.SetRemoteDescription(answer)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func TestSimpleEcho(t *testing.T) {
 	done := make(chan bool)
 	server, err := NewServer("password")
 	if err != nil {
@@ -41,5 +96,6 @@ func TestSimpleEcho(t *testing.T) {
 		}
 		done <- true
 	})
+	signalPair(client, server)
 	<-done
 }
