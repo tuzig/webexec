076a47224b6a75c10a563fadcf64e3c60b8c2a51
Benny Daon <benny@tuzig.com>
2020-04-21 16:29:12 +0300

    Accpeting CTRL-C, sort of

--- webexec.go
+++ webexec.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/afittestide/webexec/server"
 )
 
+func attachKillHandler() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\r- Ctrl+C pressed in Terminal")
+		os.Exit(0)
+	}()
+}
 func main() {
+	attachKillHandler()
 	log.Printf("Starting http server on port 8888")
 	server.HTTPGo("0.0.0.0:8888")
+	for {
+	}
 }
