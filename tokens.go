f07a01b75cb6002db70302c35121fa22c90bad65
Benny Daon <benny@tuzig.com>
2020-09-13 10:07:23 +0300

    Adding forgotton files

---
+++ tokens.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+// AddToken is used to add a new client token. The function will send the owner
+// a url he can click to approve & complete the addition
+func AddToken(c *cli.Context) error {
+	return nil
+}
+func DeleteToken(c *cli.Context) error {
+	return nil
+}
+func ListTokens(c *cli.Context) error {
+	return nil
+}
