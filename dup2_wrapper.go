cbd6386a7719d9b10ce8e512971439b023551b7f
Benny Daon <benny@tuzig.com>
2021-02-04 17:27:27 +0200

    Fixing the release action

--- dup2_wrapper.go
+++ dup2_wrapper.go
@@ -1,11 +1,9 @@
-// +build !arm
-// +build !arm64
+// +build !arm freebsd
+// +build !arm,!arm64 !linux
 
 package main
 
-import (
-	"syscall"
-)
+import "syscall"
 
 func Dup2(oldfd int, newfd int) {
 	syscall.Dup2(oldfd, newfd)
--- dup2_wrapper_armlinux.go
+++ dup2_wrapper_armlinux.go
@@ -1,11 +1,9 @@
-// +build !windows
+// +build linux
 // +build arm arm64
 
 package main
 
-import (
-	"syscall"
-)
+import "syscall"
 
 func Dup2(oldfd int, newfd int) {
 	syscall.Dup3(oldfd, newfd, 0)
