5d9b4d70fea99c70a7d3ba465bb85db999ee95e1
Benny Daon <benny@tuzig.com>
2021-03-07 11:10:39 +0200

    Adding support to [env] in webexec.conf

---
+++ pane_test.go
@@ -0,0 +1,18 @@
+// This files contains testing suites that test the conf
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecCommand(t *testing.T) {
+	initTest(t)
+	c := []string{"bash", "-c", "echo $TERM $COLORTERM"}
+	_, tty, err := execCommand(c, nil)
+	b := make([]byte, 64)
+	l, err := tty.Read(b)
+	require.Nil(t, err)
+	require.Equal(t, "xterm truecolor\r\n", string(b[:l]))
+}
