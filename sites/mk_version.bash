19ace33aa8f2465c4efcb8db48402e0355fa9c7d
Benny Daon <benny@tuzig.com>
2023-08-14 13:42:34 +0300

    Removing the 'v' from the latest version

--- sites/mk_version.bash
+++ sites/mk_version.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p sites/version
-git describe --tags $(git rev-list --tags --max-count=1) > sites/version/latest
+git describe --tags $(git rev-list --tags --max-count=1) | cut -c 2- > sites/version/latest
