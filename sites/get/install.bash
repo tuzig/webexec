f8c5dc20eadbe0326e1d7a156f64e2095f30f00c
Benny Daon <benny@tuzig.com>
2023-09-10 12:08:10 +0300

    Releasing v1.2.1

--- sites/get/install.bash
+++ sites/get/install.bash
@@ -7,7 +7,7 @@
 #   $ ./get-webexec.sh
 #
 # The latest release is currently hard-coded.
-LATEST_VERSION="1.2.0"
+LATEST_VERSION="1.2.1"
 
 echo "webexec version $LATEST_VERSION installer"
          
