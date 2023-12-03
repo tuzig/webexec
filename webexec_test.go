5ba61837ab6288df909685455761f45d7bb6acb9
Benny Daon <benny@tuzig.com>
2023-12-03 09:31:56 +0200

    Fixing a test

--- webexec_test.go
+++ webexec_test.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetVersionNote(t *testing.T) {
+	initTest(t)
+	version = "0.0.1"
 	versionNote := getVersionNote()
-	fmt.Println(versionNote)
 	require.NotEmpty(t, versionNote)
+	version = cachedVersion.version.String()
+	versionNote2 := getVersionNote()
+	require.Empty(t, versionNote2)
 }
